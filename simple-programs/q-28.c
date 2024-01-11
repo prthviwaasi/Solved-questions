520c03a0875d53ec68d8e4bf5f68e56931ba6609
Raj <prthviwaasi@gmail.com>
2024-01-11 10:12:20 +0530

    completed all question in simple programs

--- simple-programs/q-28.c
+++ simple-programs/q-28.c
@@ -2,9 +2,11 @@
 
 int main()
 {
-    int a,b;
+    int a, b, c;
     printf("Enter two numbers:\t");
     scanf("%d %d", &a, &b);
-    
+    c = a-(-b);
+    printf("Result of %d + %d = %d\n",a, b, c);
+
     return 0;
 }
--- simple-programs/q-29.c
+++ simple-programs/q-29.c
@@ -2,9 +2,13 @@
 
 int main()
 {
-    int a,b;
+    int a, b, c;
     printf("Enter two numbers:\t");
     scanf("%d %d", &a, &b);
-    
+    printf("The value of a and b before swap is %d\t%d\n",a, b);
+    a = a*b;
+    b = a/b;
+    a = a/b;
+    printf("The value of a and b after swap is %d\t%d\n",a, b);
     return 0;
 }
