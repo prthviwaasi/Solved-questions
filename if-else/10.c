d7ed28c8d89bbca2f8217faee2b537a06c2cfeff
Raj <prthviwaasi@gmail.com>
2024-01-11 11:09:08 +0530

    question no 10, 11 solved

--- if-else/10.c
+++ if-else/10.c
@@ -6,5 +6,10 @@ int main(){
     int n;
     printf("Enter any number:\t");
     scanf("%d", &n);
+    if(n%10==0)
+        printf("%d is divisible by 10\n", n);
+    else if(n = n-n%10)
+        printf("%d is the previous no. divisible by 10\n", n);
     
+    return 0;
 }
--- if-else/11.c
+++ if-else/11.c
@@ -1,8 +1,14 @@
+// .wap to enter a number if the number is grater than equal to 1000 then display 20% of the given no other wise 10% of the given no.
 #include<stdio.h>
 
 int main(){
-    int n;
+    float n;
     printf("Enter any number:\t");
-    scanf("%d", &n);
-    
+    scanf("%f", &n);
+    if(n>=1000)
+        printf("20%% of the given no. = %.2f\n", n/100*20);
+    else 
+        printf("10%% of the given no. = %.2f\n", n/100*10);
+
+    return 0;
 }
