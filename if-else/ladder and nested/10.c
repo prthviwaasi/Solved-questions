f8616f3bb209ca318705d6b937c559ebcc987f59
Raj <prthviwaasi@gmail.com>
2024-01-28 22:11:19 +0530

    Solved q-9 and q-10 from ladder and nested problem set

--- if-else/ladder and nested/10.c
+++ if-else/ladder and nested/10.c
@@ -1,1 +1,7 @@
 /*write a program to enter the month number from user & shows that how many days in this month with name.*/
+
+#include <stdio.h>
+
+int main(){
+    int a, b;
+}
--- if-else/ladder and nested/9.c
+++ if-else/ladder and nested/9.c
@@ -7,7 +7,7 @@ int main(){
     int num, choice, ans;
     printf("Enter any number?:\t");
     scanf("%d",&num);
-    printf("Choose the operation you want to perform?:\n");
+    printf("\nChoose the operation you want to perform?:\n\n");
     printf("Square(1):\n");
     printf("Cube(2):\n");
     printf("Square root(3):\n");
@@ -16,13 +16,13 @@ int main(){
 
    if(choice==1){
     ans = num*num;
-    printf("Result: %d.", ans);
+    printf("Result: %d.\n", ans);
    }else if (choice==2){
     ans = num*num*num;
-    printf("Result: %d.",  ans);
+    printf("Result: %d.\n",  ans);
    }else if (choice==3){
     ans = sqrt(num);
-    printf("Result: %d.",  ans);
+    printf("Result: %d.\n",  ans);
    }else
     printf("Invalid Choice!!\n");
 
