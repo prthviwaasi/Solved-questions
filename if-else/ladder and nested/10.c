c1ffbc3c8919adfd35318e4a765d8b9b5ed55c26
Raj Kumar <131242825+prthviwaasi@users.noreply.github.com>
2024-02-24 21:00:58 +0530

    testing...

--- if-else/ladder and nested/10.c
+++ if-else/ladder and nested/10.c
@@ -3,5 +3,60 @@
 #include <stdio.h>
 
 int main(){
-    int a, b;
+    int m;
+    printf("Enter the month number");
+    scanf("%d",&m);
+    if  (m == 1 || m==jan)
+    {
+        printf("January has 31 days");
+    }
+    else if (m == 2 || m==feb)
+    {
+        printf("February has 28 days");
+    }
+    else if (m == 3 || m==mar)
+    {
+        printf("March has 31 days");
+    }
+    else if (m == 4 || m==apr)
+    {
+        printf("April has 30 days");
+    }
+    else if (m == 5 || m==may)
+    {
+        printf("May has 31 days");
+    }
+    else if (m == 6 || m==jun)
+    {
+        printf("June has 30 days");
+    }
+    else if (m == 7 || m==jul)
+    {
+        printf("July has 31 days");
+    }
+    else if (m == 8 || m==aug)
+    {
+        printf("August has 31 days");
+    }
+    else if (m == 9 || m==sep)
+    {
+        printf("September has 30 days");
+    }
+    else if (m == 10 || m==oct)
+    {
+        printf("October has 31 days");
+    }
+    else if (m == 11 || m==nov)
+    {
+        printf("November has 30 days");
+    }
+    else if (m == 12 || m==dec)
+    {
+        printf("December has 31 days");
+    }
+    else
+    {
+        printf("Wrong choice");
+    }
+    return 0;
 }
