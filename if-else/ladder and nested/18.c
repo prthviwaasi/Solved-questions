ea254a5bdf7221d62a25534e9b98b908e635570a
prthviwaasi <prthviwaasi@gmail.com>
2024-02-25 20:16:49 +0530

    ..

--- if-else/ladder and nested/18.c
+++ if-else/ladder and nested/18.c
@@ -1,1 +1,29 @@
 /*Q(23) .wap to enter the three person height in feet and inch and display the sum of all person in feet and inch.*/
+
+#include <stdio.h>
+
+int main()
+{
+    int total_ft, total_inch, sum;
+    int feet1, inch1;
+    int feet2, inch2;
+    int feet3, inch3;
+
+    printf("Enter the first person height in (feet and inch): ");
+    scanf("%d%d", &feet1, &inch1);
+    printf("\nEnter the second person height in (feet and inch): ");
+    scanf("%d%d", &feet2, &inch2);
+    printf("\nEnter the third person height in (feet and inch): ");
+    scanf("%d%d", &feet3, &inch3);
+
+    // calculate total heigth in inches
+    total_inch = inch1 + inch2 + inch3;
+    total_ft = feet1 + feet2 + feet3 + (total_inch / 12);
+    total_inch = total_inch % 12;
+    // adjust feet and inch if total inches exceed 12
+    total_ft += total_inch / 12;
+    total_inch = total_inch % 12;
+
+    printf("Total height: %d feet and %d inches\n", total_ft, total_inch);
+    return 0;
+}
--- if-else/ladder and nested/19.c
+++ if-else/ladder and nested/19.c
@@ -1,1 +1,35 @@
-/*Q(24) .wap to enter the three person height in feet and inch and check that whose    person is tallest.*/
+/*Q(24) .wap to enter the three person height in feet and inch and check that whose person is tallest.*/
+
+#include <stdio.h>
+
+int main()
+{
+    int person_h1, person_h2, person_h3;
+    int feet1, inch1;
+    int feet2, inch2;
+    int feet3, inch3;
+
+    printf("Enter the first person height in (feet and inch): ");
+    scanf("%d%d", &feet1, &inch1);
+    printf("\nEnter the second person height in (feet and inch): ");
+    scanf("%d%d", &feet2, &inch2);
+    printf("\nEnter the third person height in (feet and inch): ");
+    scanf("%d%d", &feet3, &inch3);
+
+    // convert heights to inches for comparison
+    person_h1 = feet1 * 12 + inch1;
+    person_h2 = feet2 * 12 + inch2;
+    person_h3 = feet3 * 12 + inch3;
+
+    // Determine the tallest person
+    if (person_h1 > person_h2 && person_h1 > person_h3)
+        printf("First person is the tallest.\n");
+    else if (person_h2 > person_h1 && person_h2 > person_h3)
+        printf("Second person is the tallest.\n");
+    else if (person_h3 > person_h1 && person_h3 > person_h2)
+        printf("Third person is the tallest.\n");
+    else
+        printf("There is a tie in height.\n");
+
+    return 0;
+}
