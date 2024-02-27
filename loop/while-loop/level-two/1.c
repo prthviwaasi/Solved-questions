137373af235262e89200fc52f030bcf332c777af
Raj <prthviwaasi@gmail.com>
2024-02-27 14:06:05 +0530

    ..

--- loop/while-loop/level-two/1.c
+++ loop/while-loop/level-two/1.c
@@ -1,7 +1,22 @@
 // write a program to enter any number check that the number is perfect or not.
 #include <stdio.h>
-int main(){
-    int ;
+int main()
+{
+    int n, i = 1, s = 0;
+    printf("Enter any number: ");
+    scanf("%d", &n);
+    while (i < n)
+    {
+        if (n % i == 0)
+        {
+            s++;
+        }
+        i++;
+    }
+    if (s == n)
+        printf("Number is perfect");
+    else
+        printf("Number is not perfect");
 
-return 0;
+    return 0;
 }
